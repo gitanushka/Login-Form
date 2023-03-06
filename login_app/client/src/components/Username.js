a53da38bc7b54fffe7757af18b4b37c89c3cc2aa
gitanushka <77441008+gitanushka@users.noreply.github.com>
2023-03-06 16:31:32 +0530

    refactor: Username.js file and App.js file updated

--- login_app/client/src/components/Username.js
+++ login_app/client/src/components/Username.js
@@ -1,7 +1,39 @@
 import React from 'react'
+// single page applications do not reload browser
+import {Link} from 'react-router-dom'
+import avatar from '../assets/profile.png';
+
+import styles from '../styles/Username.Module.css';
 
 export default function Username() {
   return (
-    <div>Username</div>
+      <div className="container mx-auto">
+        <div className='flex h-screen'>
+          <div>
+            <div className="title flex flex-col items-center">
+                <h4 className='text-5xl font-bold'>Hello Again!</h4>
+                <span className='py-4 text-xl w-2/3 text-center text-gray-500'>
+                  Explore More by connecting with us.
+                </span>
+            </div>
+
+             <form className='py-1'>
+                <div className='profile flex justify-center py-4'>
+                   <img src={avatar} alt='avatar'></img>
+                </div>
+
+                <div className='textbox flex flex-col items-center gap-6'>
+                  <input type='text' placeholder='Username'></input>
+                  <button type='submit'>Let's Go</button>
+                </div>
+
+                <div className='text-center py-4'> 
+                   <span className='text-gray-500'>Not a Member? <Link className='text-red-500' to='/register'>Register Now</Link></span>
+                </div>
+             </form>
+
+          </div>
+        </div>
+      </div>
   )
 }
