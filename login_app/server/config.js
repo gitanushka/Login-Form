633773b93a45138301ed82f1238ab731dcc4cedc
gitanushka <77441008+gitanushka@users.noreply.github.com>
2023-03-08 16:49:26 +0530

    refactor: config file

--- login_app/server/config.js
+++ login_app/server/config.js
@@ -1,5 +1,5 @@
 export default {
     JWT_SECRET: "uyLb43ZCJ8MUnwx0jQLfZzEyT0WuohP3+mRZdDFVV6I=",
-    EMAIL: "lorna.sipes@ethereal.email",
+    EMAIL: "lorna.sipes@ethereal.email", //testing email and password
     PASSWORD: "hsnz19YcA9tFxvYvMq"
 }
