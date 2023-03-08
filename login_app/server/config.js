b9a1d95a4807869099ddd3ec73c7d2afc71d7366
gitanushka <77441008+gitanushka@users.noreply.github.com>
2023-03-08 16:47:33 +0530

    chore: server files

--- login_app/server/config.js
+++ login_app/server/config.js
@@ -1,3 +1,5 @@
 export default {
-    JWT_SECRET: "uyLb43ZCJ8MUnwx0jQLfZzEyT0WuohP3+mRZdDFVV6I="
+    JWT_SECRET: "uyLb43ZCJ8MUnwx0jQLfZzEyT0WuohP3+mRZdDFVV6I=",
+    EMAIL: "lorna.sipes@ethereal.email",
+    PASSWORD: "hsnz19YcA9tFxvYvMq"
 }
