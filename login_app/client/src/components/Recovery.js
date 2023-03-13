77a0e39f658dcb2b44626f955273222e1c50e0f7
gitanushka <77441008+gitanushka@users.noreply.github.com>
2023-03-13 23:27:16 +0530

    refactor: connecting files with db

--- login_app/client/src/components/Recovery.js
+++ login_app/client/src/components/Recovery.js
@@ -13,7 +13,6 @@ export default function Recovery() {
 
   useEffect(() => {
     generateOTP(username).then((OTP) => {
-      console.log(OTP)
       if(OTP) return toast.success('OTP has been send to your email!');
       return toast.error('Problem while generating OTP!')
     })
--- login_app/server/config.js
+++ login_app/server/config.js
@@ -1,5 +1,6 @@
 export default {
     JWT_SECRET: "uyLb43ZCJ8MUnwx0jQLfZzEyT0WuohP3+mRZdDFVV6I=",
     EMAIL: "lorna.sipes@ethereal.email", //testing email and password
-    PASSWORD: "hsnz19YcA9tFxvYvMq"
+    PASSWORD: "hsnz19YcA9tFxvYvMq",
+    ATLAS_URI: "mongodb+srv://anushkasharma:101202@cluster0.rcwrcvc.mongodb.net/?retryWrites=true&w=majority"
 }
